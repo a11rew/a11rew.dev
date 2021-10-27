827184b6654748c417922517837bd7ab080d0622
AndrewGlago <andrewglago1@gmail.com>
2021-10-27 17:16:00 +0000

    global styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,19 @@
+import React from 'react'
+import GlobalStyles from './src/styles.globals'
+
 // custom typefaces
 import '@fontsource/inter'
 // normalize CSS across browsers
 import './src/normalize.css'
 // custom CSS styles
-import './src/style.css'
+// import './src/style.css'
 
 // Highlighting for code blocks
 import 'prismjs/themes/prism.css'
+
+export const wrapRootElement = ({ element }) => (
+  <div id="root">
+    <GlobalStyles />
+    {element}
+  </div>
+)
