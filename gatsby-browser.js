033c4823a397d0a7481eac2fdab975e6d035440e
AndrewGlago <andrewglago1@gmail.com>
2021-10-27 17:17:45 +0000

    global styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,7 +6,6 @@ import '@fontsource/inter'
 // normalize CSS across browsers
 import './src/normalize.css'
 // custom CSS styles
-// import './src/style.css'
 
 // Highlighting for code blocks
 import 'prismjs/themes/prism.css'
