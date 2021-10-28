3153d666a2a40380bfd9deca60f56d8849237fc3
AndrewGlago <andrewglago1@gmail.com>
2021-10-28 17:29:23 +0000

    move global styles out of js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,10 +3,11 @@ import GlobalStyles from './src/styles.globals'
 
 // custom typefaces
 import '@fontsource/inter'
+import '@fontsource/montserrat'
 // normalize CSS across browsers
 import './src/normalize.css'
 // custom CSS styles
-
+import './src/index.css'
 // Highlighting for code blocks
 import 'prismjs/themes/prism.css'
 
