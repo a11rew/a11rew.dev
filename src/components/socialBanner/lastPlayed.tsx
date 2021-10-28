79d9c31b9d936eb7a9cb130fffd82360f748bee5
AndrewGlago <andrewglago1@gmail.com>
2021-10-28 17:30:44 +0000

    spotify icon alt

--- src/components/socialBanner/lastPlayed.tsx
+++ src/components/socialBanner/lastPlayed.tsx
@@ -49,6 +49,10 @@ const LastPlayed: React.FC = (): ReactElement => {
       <div>
         <h5>Last Played</h5>
         <a href={songData?.songHref} target="_blank" rel="noreferrer noopener">
+          <HiddenDescription>
+            Last.fm link to the song {songData?.songTitle} by{' '}
+            {songData?.songArtist}{' '}
+          </HiddenDescription>
           <BsSpotify size={20} />
         </a>
       </div>
@@ -124,6 +128,29 @@ const SongAlbum = styled.span`
 
 const SongArtist = styled.span`
   font-size: var(--fontSize-0);
+`
+
+const HiddenDescription = styled.span`
+  border: 0;
+  padding: 0;
+  margin: 0;
+  position: absolute !important;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(
+    1px 1px 1px 1px
+  ); /* IE6, IE7 - a 0 height clip, off to the bottom right of the visible 1px box */
+  clip: rect(
+    1px,
+    1px,
+    1px,
+    1px
+  ); /*maybe deprecated but we need to support legacy browsers */
+  clip-path: inset(
+    50%
+  ); /*modern browsers, clip-path works inwards from each corner*/
+  white-space: nowrap; /* added line to stop words getting smushed together (as they go onto seperate lines and some screen readers do not understand line feeds as a space */
 `
 
 export default LastPlayed
