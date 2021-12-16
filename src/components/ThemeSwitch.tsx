06ec3aa8e53abbd0f49afec6425c54a51dbf21a9
Andrew Glago <andrewglago1@gmail.com>
2021-12-16 13:33:13 +0000

    ssr window check

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -7,15 +7,12 @@ import { FiMoon } from 'react-icons/fi'
 
 const ThemeSwitch: React.FC = (): React.ReactElement => {
   // Use user theme preference
-  const prefersDark = () => {
-    if (typeof window !== undefined) {
-      return window.matchMedia('(prefers-color-scheme: dark)').matches
-    }
-    return null
-  }
+  const prefersDark =
+    typeof window !== 'undefined' &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
 
   const [theme, setTheme] = useState<'dark' | 'light'>(
-    prefersDark() ? 'dark' : 'light'
+    prefersDark ? 'dark' : 'light'
   )
 
   const handleSwitch = (): void => {
