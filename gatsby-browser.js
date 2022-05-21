835ec8cebca3dcb1c56131b20f5d6d236a24e457
Andrew Glago <andrewglago1@gmail.com>
2022-05-21 15:14:25 +0000

    Switch to Articulat font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import GlobalStyles from './src/styles.globals'
 
-// custom typefaces
-import '@fontsource/inter'
-import '@fontsource/montserrat'
+// typography
+import './src/typography.css'
+
 // normalize CSS across browsers
 import './src/normalize.css'
 // custom CSS styles
