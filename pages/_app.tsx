cf808a418ed8e9d6d82a71aeccf695999c69b4ed
Andrew Glago <andrewglago1@gmail.com>
2022-09-10 12:09:18 +0000

    Setup Satoshi font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+import "../styles/fonts/satoshi.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
