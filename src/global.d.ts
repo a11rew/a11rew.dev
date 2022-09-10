6680b3bc841005951f6b5b7d509669619fd468e7
Andrew Glago <andrewglago1@gmail.com>
2022-09-10 11:52:13 +0000

    Setup v2 base

--- src/global.d.ts
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line no-underscore-dangle
-declare const __PATH_PREFIX__: string
--- src/utils/scrollTo.ts
+++
@@ -1,8 +0,0 @@
-const scrollTo = (target: string): void => {
-  document.querySelector(`${target}`)?.scrollIntoView({
-    behavior: 'smooth',
-    block: window.innerWidth < 425 ? 'start' : 'center',
-  })
-}
-
-export default scrollTo
