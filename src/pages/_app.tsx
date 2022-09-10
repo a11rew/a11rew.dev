ef98da23ee84993e30db6954855eeeb5fff07265
Andrew Glago <andrewglago1@gmail.com>
2022-09-10 13:33:06 +0000

    Setup SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 
-import "../styles/globals.css";
-import "../styles/fonts/satoshi.css";
+import "@/styles/globals.css";
+import "@/styles/fonts/satoshi.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import SEO from "@/components/SEO";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen text-theme-text-white bg-theme-bg-black">
+      <SEO />
+      <main>Main</main>
+    </div>
+  );
+}
